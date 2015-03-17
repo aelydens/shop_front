d9e0b349441f3f75a4a026d557374219e301ac44
Annie Lydens <aelydens@gmail.com>
2015-03-17 14:25:34 -0600

    User auth controllers

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,18 @@
+class RegistrationsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(params.require(:user).permit(:username, :password))
+    if user.save
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome, #{user.username}"
+      redirect_to products_path
+    else
+      flash[:notice] = "Oops, something went wrong!"
+      render :new
+    end
+  end
+
+end
