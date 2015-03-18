1c3aee5e5024aff62f326646a11563a11af37f39
Annie Lydens <aelydens@gmail.com>
2015-03-17 20:36:19 -0600

    Fix minor error w/ registrations form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,10 +4,10 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-    user = User.new(params.require(:user).permit(:username, :password))
-    if user.save
-      session[:user_id] = user.id
-      flash[:notice] = "Welcome, #{user.username}"
+    @user = User.new(params.require(:user).permit(:username, :password))
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome, #{@user.username}"
       redirect_to products_path
     else
       flash[:notice] = "Oops, something went wrong!"
