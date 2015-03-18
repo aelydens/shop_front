fbb2f431b1fbb1207282f32b71174bf3dc255727
Annie Lydens <aelydens@gmail.com>
2015-03-17 20:46:31 -0600

    Add product helper method to print out of stock message

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,4 +2,13 @@ module ProductsHelper
   def print_price(price)
     format("$%.2f", price)
   end
+
+  def print_quantity(quantity)
+    if quantity =< 0
+      content_tag("Out of Stock")
+    else
+      content_tag("In Stock", quantity)
+    end
+  end
+
 end
